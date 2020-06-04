233faa5ff434d9ea4908d3b83e59dc70c2fea149
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2020-06-04 13:55:14 +0100

    Declaration and confirmation.

---
+++ app/models/http/TRNResponse.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.http
+
+import play.api.Logger
+import play.api.http.Status._
+import play.api.libs.json._
+import uk.gov.hmrc.http.{HttpReads, HttpResponse}
+
+sealed trait DeclarationResponse
+
+final case class TRNResponse(trn : String) extends DeclarationResponse
+
+object TRNResponse {
+
+  implicit val formats : OFormat[TRNResponse] = Json.format[TRNResponse]
+
+}
+
+object DeclarationResponse {
+
+  implicit object RegistrationResponseFormats extends Reads[DeclarationResponse] {
+
+    override def reads(json: JsValue): JsResult[DeclarationResponse] = json.validate[TRNResponse]
+
+  }
+
+  case object InternalServerError extends DeclarationResponse
+
+  implicit lazy val httpReads: HttpReads[DeclarationResponse] =
+    new HttpReads[DeclarationResponse] {
+      override def read(method: String, url: String, response: HttpResponse): DeclarationResponse = {
+        Logger.info(s"[DeclarationResponse] response status received from estates api: ${response.status}")
+
+        response.status match {
+          case OK =>
+            response.json.as[TRNResponse]
+          case _ =>
+            InternalServerError
+        }
+      }
+    }
+
+
+}
