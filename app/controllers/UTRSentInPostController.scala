98a9c53e77866f1b7c7c5b4b9e2140243cc8de9c
Jay Nabonne <jaynabonne@gmail.com>
2020-06-18 14:47:37 +0100

    TRUS-2762: UTR Sent in Post view.

--- app/controllers/UTRSentInPostController.scala
+++ app/controllers/UTRSentInPostController.scala
@@ -20,12 +20,12 @@ import com.google.inject.{Inject, Singleton}
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendBaseController
-import views.html.FeatureNotAvailableView
+import views.html.UTRSentInPostView
 
 @Singleton
 class UTRSentInPostController @Inject()(
                                                val controllerComponents: MessagesControllerComponents,
-                                               view: FeatureNotAvailableView
+                                               view: UTRSentInPostView
                                              ) extends FrontendBaseController with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = Action { implicit request =>
