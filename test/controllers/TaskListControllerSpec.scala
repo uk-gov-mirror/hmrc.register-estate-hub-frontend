2b2e340e5891a6c7e6c58f0c36a989849546dd29
Adam Conder <adamdconder@gmail.com>
2021-04-27 09:59:16 +0100

    TRUS-3175 Fix tests for task list
    - Removing old a11y link
    - Play-ui now configures the link for /estates

--- test/controllers/TaskListControllerSpec.scala
+++ test/controllers/TaskListControllerSpec.scala
@@ -40,19 +40,9 @@ class TaskListControllerSpec extends SpecBase {
   private val estateDetailsRoute: String = "http://localhost:8823/register-an-estate/details"
   private val personalRepRoute: String = "http://localhost:8825/register-an-estate/personal-representative"
   private val deceasedPersonsRoute: String = "http://localhost:8824/register-an-estate/deceased-person"
-  private val registerTaxRoute: String = "http://localhost:8827/register-tax-liability"
-  private val featureUnavailableRoute: String = "http://localhost:8822/register-an-estate/feature-not-available"
-
-  private val mockAppConfig: FrontendAppConfig = mock[FrontendAppConfig]
+  private val registerTaxRoute: String = "http://localhost:8827/register-an-estate/tax-liability"
 
   private val mockRepository : SessionRepository = mock[SessionRepository]
-
-  when(mockAppConfig.estateDetailsFrontendUrl) thenReturn estateDetailsRoute
-  when(mockAppConfig.personalRepFrontendUrl) thenReturn personalRepRoute
-  when(mockAppConfig.deceasedPersonsFrontendUrl) thenReturn deceasedPersonsRoute
-  when(mockAppConfig.registerTaxFrontendUrl) thenReturn registerTaxRoute
-  when(mockAppConfig.featureUnavailableUrl) thenReturn featureUnavailableRoute
-  when(mockAppConfig.analyticsToken) thenReturn "N/A"
 
   private val mockEstatesStoreConnector: EstatesStoreConnector = mock[EstatesStoreConnector]
   private val mockEstatesConnector: EstatesConnector = mock[EstatesConnector]
@@ -79,16 +69,10 @@ class TaskListControllerSpec extends SpecBase {
 
         when(mockEstatesConnector.getIsLiableForTax()(any(), any())).thenReturn(Future.successful(true))
 
-        when(mockAppConfig.estateDetailsEnabled) thenReturn true
-        when(mockAppConfig.personalRepEnabled) thenReturn true
-        when(mockAppConfig.deceasedPersonsEnabled) thenReturn true
-        when(mockAppConfig.registerTaxEnabled) thenReturn true
-
         val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
           .overrides(Seq(
             bind(classOf[EstatesStoreConnector]).toInstance(mockEstatesStoreConnector),
             bind(classOf[EstatesConnector]).toInstance(mockEstatesConnector),
-            bind[FrontendAppConfig].toInstance(mockAppConfig),
             bind[SessionRepository].toInstance(mockRepository)
           ))
           .build()
@@ -102,52 +86,7 @@ class TaskListControllerSpec extends SpecBase {
         status(result) mustEqual OK
 
         contentAsString(result) mustEqual
-          view(None, sections, isTaskListComplete = true, Organisation)(fakeRequest, messages).toString
-
-        application.stop()
-      }
-
-      "no routes enabled" in {
-
-        val sections = List(
-          Task(Link(EstateName, featureUnavailableRoute), Some(Completed)),
-          Task(Link(PersonalRepresentative, featureUnavailableRoute), Some(Completed)),
-          Task(Link(PersonWhoDied, featureUnavailableRoute), Some(Completed))
-        )
-
-        when(mockRepository.set(any())).thenReturn(Future.successful(true))
-
-        when(mockEstatesStoreConnector.getStatusOfTasks(any(), any()))
-          .thenReturn(Future.successful(CompletedTasks(details = true, personalRepresentative = true, deceased = true, yearsOfTaxLiability = false)))
-
-        when(mockEstatesConnector.getEstateName()(any(), any())).thenReturn(Future.successful(None))
-
-        when(mockEstatesConnector.getIsLiableForTax()(any(), any())).thenReturn(Future.successful(false))
-
-        when(mockAppConfig.estateDetailsEnabled) thenReturn false
-        when(mockAppConfig.personalRepEnabled) thenReturn false
-        when(mockAppConfig.deceasedPersonsEnabled) thenReturn false
-        when(mockAppConfig.registerTaxEnabled) thenReturn false
-
-        val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
-          .overrides(Seq(
-            bind(classOf[EstatesStoreConnector]).toInstance(mockEstatesStoreConnector),
-            bind(classOf[EstatesConnector]).toInstance(mockEstatesConnector),
-            bind[FrontendAppConfig].toInstance(mockAppConfig),
-            bind[SessionRepository].toInstance(mockRepository)
-          ))
-          .build()
-
-        val request = FakeRequest(GET, controllers.registration_progress.routes.TaskListController.onPageLoad().url)
-
-        val result = route(application, request).value
-
-        val view = application.injector.instanceOf[TaskListView]
-
-        status(result) mustEqual OK
-
-        contentAsString(result) mustEqual
-          view(None, sections, isTaskListComplete = true, Organisation)(fakeRequest, messages).toString
+          view(None, sections, isTaskListComplete = true, Organisation)(request, messages).toString
 
         application.stop()
       }
@@ -168,16 +107,10 @@ class TaskListControllerSpec extends SpecBase {
         when(mockEstatesConnector.getEstateName()(any(), any())).thenReturn(Future.successful(None))
         when(mockEstatesConnector.getIsLiableForTax()(any(), any())).thenReturn(Future.successful(false))
 
-        when(mockAppConfig.estateDetailsEnabled) thenReturn true
-        when(mockAppConfig.personalRepEnabled) thenReturn true
-        when(mockAppConfig.deceasedPersonsEnabled) thenReturn true
-        when(mockAppConfig.registerTaxEnabled) thenReturn true
-
         val application = applicationBuilder(userAnswers = Some(emptyUserAnswers))
           .overrides(Seq(
             bind(classOf[EstatesStoreConnector]).toInstance(mockEstatesStoreConnector),
             bind(classOf[EstatesConnector]).toInstance(mockEstatesConnector),
-            bind[FrontendAppConfig].toInstance(mockAppConfig),
             bind[SessionRepository].toInstance(mockRepository)
           ))
           .build()
@@ -191,7 +124,7 @@ class TaskListControllerSpec extends SpecBase {
         status(result) mustEqual OK
 
         contentAsString(result) mustEqual
-          view(None, sections, isTaskListComplete = false, Organisation)(fakeRequest, messages).toString
+          view(None, sections, isTaskListComplete = false, Organisation)(request, messages).toString
 
         application.stop()
       }
