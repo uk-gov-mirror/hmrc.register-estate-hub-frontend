fe6433aefa9dc0d5d4be3f74e731bf421c7fd8a9
MRBeresford <125281117+MRBeresford@users.noreply.github.com>
2023-09-06 13:59:52 +0100

    DDCE-4195: Update CSP policy (#112)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,16 @@ import sbt.*
 
 object AppDependencies {
 
-  val boostrapVersion = "7.20.0"
+  val boostrapVersion = "7.21.0"
   val mongoVersion = "1.3.0"
 
   private lazy val compile = Seq(
-    play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.15.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"             %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % boostrapVersion,
-    "uk.gov.hmrc"             %% "tax-year"                       % "3.2.0"
+    "uk.gov.hmrc"             %% "tax-year"                       % "3.3.0"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
@@ -21,8 +20,8 @@ object AppDependencies {
     "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.16.0",
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
     "org.scalatest"               %% "scalatest"                % "3.2.16",
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
-    "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.22",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.0.0",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
