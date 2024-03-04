fd6c18e4001d78cad87cee3d4f9d6a18b4ffaaf2
garytate123 <garytatelufc@gmail.com>
2024-03-04 11:40:30 +0000

    DDCE-5382 Dependency updates for Tudor crown change

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,21 +7,21 @@ object AppDependencies {
 
   private lazy val compile = Seq(
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.23.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-28"    % "8.5.0",
     "uk.gov.hmrc"             %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % boostrapVersion,
-    "uk.gov.hmrc"             %% "tax-year"                       % "3.3.0"
+    "uk.gov.hmrc"             %% "tax-year"                       % "4.0.0"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-28"   % boostrapVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-28"  % mongoVersion,
-    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.17.0",
-    "org.jsoup"                   %  "jsoup"                    % "1.16.1",
-    "org.scalatest"               %% "scalatest"                % "3.2.17",
-    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.27",
-    "org.wiremock"                %  "wiremock-standalone"      % "3.2.0",
+    "org.scalatestplus"           %% "scalacheck-1-17"          % "3.2.18.0",
+    "org.jsoup"                   %  "jsoup"                    % "1.17.2",
+    "org.scalatest"               %% "scalatest"                % "3.2.18",
+    "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.30",
+    "org.wiremock"                %  "wiremock-standalone"      % "3.4.2",
     "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
